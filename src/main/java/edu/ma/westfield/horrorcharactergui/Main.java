0673087e56789288f96ad8faeab07c3d318f8d8b
Kanadox <kanadox.mail@gmail.com>
2025-11-23 14:02:27 -0500

    Added scene switching

--- src/main/java/edu/ma/westfield/horrorcharactergui/Main.java
+++ src/main/java/edu/ma/westfield/horrorcharactergui/Main.java
@@ -7,8 +7,11 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class Main extends Application {
+    private static Stage currentStage;
+
     @Override
     public void start(Stage stage) throws IOException {
+        currentStage = stage;
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("ListView.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 620, 440);
         stage.setTitle("Horror Character List");
@@ -16,6 +19,12 @@ public class Main extends Application {
         stage.show();
     }
 
+    public static void setScene(String fxmlFile) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource(fxmlFile));
+        Scene scene = new Scene(fxmlLoader.load(), 620, 440);
+        currentStage.setScene(scene);
+        currentStage.show();
+    }
 
     public static void main(String[] args) {
         launch();
